da1b39e30023316d8d9256838c0c0bfcaaca7c1e
Andreas Linz <anli@spreadshirt.net>
2020-10-17 13:05:10 +0200

    Remove debug log

--- assets/base.js
+++ assets/base.js
@@ -1,7 +1,6 @@
 let el = document.getElementById("note");
 if (el) {
   el.addEventListener("keydown", function (e) {
-    console.log(e);
     if (e.ctrlKey && e.key === "Enter") {
       this.form.submit();
     }
